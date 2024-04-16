b5c1290fd6c95035cf3f3f2ba1a1845a51a7baf0
aashish00021 <aashishbhardwaj07@icloud.com>
2024-04-16 11:03:56 +0530

    Q-1295
    Simple solution for question 1295

---
+++ evenDigits.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class evenDigits {
+    public static void main(String[] args){
+        int[] nums = {12,345,2,6,7896};
+        int ans = findNumbers(nums);
+        System.out.println(ans);
+    }
+
+    static int findNumbers(int[] nums){
+        int count = 0;
+        for(int num : nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean even(int num){
+        int noOfDigits = digits(num);
+        if(noOfDigits % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    static int digits(int num){
+        int count = 0;
+        while(num > 0){
+            count ++;
+            num = num / 10;
+        }
+        return count;
+    }
+}
